da71082f2a1bb0d28621ba75f91546fd2ca58681
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-05 06:58:40 -0400

    Adds create_app app factory function.

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#   ___ ___  _      _
+#  / __| _ \/_\ ___| |__  __ _ ___ ___
+#  \__ \  _/ _ \___| '_ \/ _` (_-</ -_)
+#  |___/_|/_/ \_\  |_.__/\__,_/__/\___|
+#
+
+"""
+app
+~~~
+
+The flask app for spa-base
+"""
+
+from .app import create_app
+
+
+__title__      = 'spa-base'
+__version__    = '0.0.1'
+__author__     = 'Harold Bradley III'
+__license__    = 'MIT'
+__copyright__  = 'Copyright 2018 Harold Bradley III'
+
+# Soli Deo gloria. <><
---
+++ app/app.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+app.app
+~~~~~~~
+
+The app factory for spa-base
+"""
+
+from os import path
+
+from dotenv import load_dotenv
+from flask import Flask, redirect, request, render_template
+from werkzeug.utils import find_modules, import_string
+
+
+def create_app(Config = None):
+    """Flask app factory function."""
+    load_dotenv()
+    app = Flask(__name__)
+    register_blueprints(app)
+
+    if app.config['FLASK_ENV'] == 'development':
+        @app.before_request
+        def route_static_files():
+            """This function routes static files appropriately when in development
+            mode (flask run).
+
+            Rather than forcing all static files to be in the 'static' directory,
+            this allows the possiblity of serving files from the root. This is
+            quite easy with Apache or Nginx, but when running via `flask run`
+            (assuming FLASK_ENV is set to 'development'), this function is
+            necessary. It first checks to see if a path is a real file, then it
+            redirects to the static soft link which Flask can handle
+            appropriately.
+            """
+            basedir = path.abspath(path.dirname(__file__))
+            request_path = request.path
+            if request_path[0] == '/':
+                request_path = request_path[1:]
+            if path.isfile(basedir + '/static/' + request_path):
+                return redirect('/static/' + request_path, 307)
+
+    return app
+
+
+def register_blueprints(app):
+    """Automatically registers all blueprints that are in the app.modules
+    package.
+
+    @see https://speakerdeck.com/player/94a53afb6a524ad88f301f62166a27e4
+    """
+    for name in find_modules('app.modules', include_packages=True, recursive=True):
+        mod = import_string(name)
+        if hasattr(mod, 'blueprint'):
+            app.register_blueprint(mod.blueprint)
