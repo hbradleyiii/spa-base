1351a14c9d6df67643286319163405efcef09734
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-06 21:29:36 -0400

    Adds pages blueprint.

---
+++ app/blueprints/pages/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+app.blueprints.pages
+~~~~~~~~~~~~~~~~~~~~
+
+The static pages blueprint for spa-base.
+"""
+
+from .routes import blueprint
---
+++ app/blueprints/pages/routes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+app.blueprints.pages.routes
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The pages routes for spa-base
+"""
+
+from flask import Blueprint, render_template
+
+
+blueprint = Blueprint('pages', __name__, template_folder='templates')
+
+
+@blueprint.route('/terms-and-conditions/')
+def terms_and_conditions():
+    return render_template('pages/terms-and-conditions.html',
+                           title='Terms and Conditions')
+
+
+@blueprint.route('/privacy-policy/')
+def privacy_policy():
+    return render_template('pages/privacy-policy.html', title='Privacy Policy')
