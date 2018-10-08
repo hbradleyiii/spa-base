3b9fe419d88287d64b3691d8bb70de3a82c45c82
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-07 20:17:12 -0400

    Adds error handlers in routes.

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ The main routes for spa-base.
 
 from flask import render_template
 from flask_login import login_required
+from .models import db
 
 
 def init_app(app):
@@ -19,3 +20,14 @@ def init_app(app):
     @login_required
     def index(path=None):
         return render_template('index.html', path=path)
+
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('404.html'), 404
+
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        db.session.rollback()
+        return render_template('500.html'), 500
