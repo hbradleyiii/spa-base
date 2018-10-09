79380c5f55bd97a491f4e6a5ab3cc2d47de2c495
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-08 22:09:34 -0400

    Adds a helpful sendmail function.

--- app/mail.py
+++ app/mail.py
@@ -7,10 +7,17 @@ app.mail
 The mail module for spa-base.
 """
 
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
 
 mail = Mail()
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
 
 
 def init_app(app):
