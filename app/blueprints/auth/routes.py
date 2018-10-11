c0d5670039bcc20000684470f4a991b85e144a98
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-10 22:26:59 -0400

    Adds password reset routes.

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -20,7 +20,13 @@ from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
 from app.models import db, User
-from .forms import LoginForm, RegistrationForm
+from .forms import (
+    LoginForm,
+    PasswordResetForm,
+    RegistrationForm,
+    RequestPasswordResetForm,
+)
+from .mail import send_password_reset_mail
 
 
 blueprint = Blueprint('auth', __name__, template_folder='templates')
@@ -50,6 +56,37 @@ def logout():
         abort(405)
     logout_user()
     return redirect(url_for('auth.login'))
+
+
+@blueprint.route('/request_password_reset', methods=['GET', 'POST'])
+def request_password_reset():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RequestPasswordResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_mail(user)
+            flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/request_password_reset.html',
+                           title='Reset Password', form=form)
+
+
+@blueprint.route('/reset_password/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_password_reset_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = PasswordResetForm()
+    if form.validate_on_submit():
+        user.password = form.password.data
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/password_reset.html', form=form)
 
 
 @blueprint.route('/register/', methods=['GET', 'POST'])
