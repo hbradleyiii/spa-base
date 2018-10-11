e4307952183a1b02b90e8ea4d92c82641e71205d
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-10 22:19:17 -0400

    Changes send_email to send_mail for consistency.

--- app/mail.py
+++ app/mail.py
@@ -13,7 +13,7 @@ from flask_mail import Mail, Message
 mail = Mail()
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_mail(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
