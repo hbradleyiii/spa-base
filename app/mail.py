47c897dc22c674840ef1a4495d3a7a6dc8ad12fd
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-13 14:04:20 -0400

    Adds ability to send mail in a different thread. (asynchronously)

--- app/mail.py
+++ app/mail.py
@@ -7,17 +7,30 @@ app.mail
 The mail module for spa-base.
 """
 
+from threading import Thread
+
+from flask import current_app
 from flask_mail import Mail, Message
 
 
 mail = Mail()
 
 
-def send_mail(subject, sender, recipients, text_body, html_body):
+def send_mail(subject, sender, recipients, text_body, html_body,
+              send_async=True):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    if send_async:
+        Thread(target=send_mail_async, args=(current_app._get_current_object(),
+                                             msg)).start()
+    else:
+        mail.send(msg)
+
+
+def send_mail_async(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def init_app(app):
