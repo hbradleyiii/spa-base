077ca088fcb336fbd58fc72cd5e413131afefe59
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-15 07:12:36 -0400

    Refacters the fixtures and adds a session fixture.

--- tests/utilities/fixtures.py
+++ tests/utilities/fixtures.py
@@ -11,7 +11,7 @@ import pytest
 
 
 @pytest.fixture(scope='module')
-def app(request):
+def app():
     """Creates and returns an app with its context."""
     from app import create_app
     from app.config import TestingConfig
@@ -19,25 +19,50 @@ def app(request):
     app = create_app(TestingConfig)
     ctx = app.app_context()
     ctx.push()
-    request.addfinalizer(ctx.pop)
 
-    return app
+    yield app
+
+    ctx.pop()
 
 
-@pytest.fixture(scope='function')
-def db(app, request):
+@pytest.fixture(scope='module')
+def db(app):
+    """Creates a new database for each test module."""
     from app.models import db
 
     db.create_all()
+
     yield db
+
     db.session.remove()
     db.drop_all()
 
 
+@pytest.fixture(scope='function')
+def session(db):
+    """Creates a new database session for a test.
+    @see http://alexmic.net/flask-sqlalchemy-pytest/"""
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    options = dict(bind=connection, binds={})
+    session = db.create_scoped_session(options=options)
+
+    db.session = session
+
+    yield session
+
+    transaction.rollback()
+    connection.close()
+    session.remove()
+
+
 @pytest.fixture()
-def client(request, app):
+def client(app):
     """Creates and returns an app client for testing."""
     client = app.test_client()
     client.__enter__()
-    request.addfinalizer(lambda: client.__exit__(None, None, None))
-    return client
+
+    yield client
+
+    client.__exit__(None, None, None)
