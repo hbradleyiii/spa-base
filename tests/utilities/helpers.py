e51cd3043c9335cbe36ca276baf5a211fb4ad21a
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-20 22:18:37 -0400

    Fixes a bug in create_user function.

--- tests/utilities/helpers.py
+++ tests/utilities/helpers.py
@@ -10,11 +10,11 @@ Helper functions for tests.
 from app.models import User
 
 
-def create_user(db, first_name='Jane', last_name='Doe', password='password123',
+def create_user(session, first_name='Jane', last_name='Doe', password='password123',
                 email='jane@example.com', emails=[]):
     """Helper function to create a user for the app."""
     user = User(first_name=first_name, last_name=last_name, password=password,
                 email=email, emails=emails)
-    db.session.add(user)
-    db.session.commit()
+    session.add(user)
+    session.flush()
     return user
