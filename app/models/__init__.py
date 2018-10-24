06f66690c45673e7ff07e92ce121c3470805c593
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-23 21:53:38 -0400

    Adds automatic commit after successful requests.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,11 +7,24 @@ app.models
 The models module for spa-base.
 """
 
+from sqlalchemy.exc import DatabaseError
 
-from .base import BaseModel, db, migrate
-from .user import User
+from .base import BaseModel, db, migrate, session
+from .user import DuplicateEmailException, User
 
 
 def init_app(app):
     db.init_app(app)
     migrate.init_app(app, db)
+
+    @app.after_request
+    def session_commit(response):
+        """Automatically commit database changes at the end of every successful
+        request."""
+        if response.status_code >= 400:
+            return
+        try:
+            session.commit()
+        except DatabaseError:
+            session.rollback()
+            raise
