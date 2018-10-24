8822c9df1924f8af9506ee59c3c1aa1315285d44
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-23 21:47:28 -0400

    Updates the Base model with more methods and better structure.

--- app/models/base.py
+++ app/models/base.py
@@ -5,12 +5,18 @@ app.models.base
 ~~~~~~~~~~~~~~~
 
 The db and migrate objects and the BaseModel abstract class for spa-base.
+
+Much of the base model has been derived from:
+https://chase-seibert.github.io/blog/2016/03/31/flask-sqlalchemy-sessionless.html
 """
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import DatabaseError
+
 
 db = SQLAlchemy()
+session = db.session
 migrate = Migrate()
 
 
@@ -20,10 +26,25 @@ class BaseModel(db.Model):
     def save(self):
         """Flushes this model to the database (as well as any other pending
         changes)."""
-        db.session.add(self)
-        db.session.commit()
+        session.add(self)
+        self._flush()
+        return self
+
+    def update(self, **kwargs):
+        """Updates the model with provided attributes."""
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return self.save()
 
     def delete(self):
         """Deletes this model instance."""
-        db.session.delete(self)
-        db.session.commit()
+        session.delete(self)
+        self._flush()
+
+    def _flush(self):
+        """Flushes any session changes to the database."""
+        try:
+            session.flush()
+        except DatabaseError:
+            session.rollback()
+            raise
