0b5254924d3d8e7183a86886515624d1a6f678ae
Harold Bradley III <hbradleyiii@bradleystudio.net>
2018-10-27 07:13:32 -0400

    Updates the auth blueprint's routes to use new Email model.

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -19,7 +19,7 @@ from flask import (
 from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
-from app.models import db, User
+from app.models import db, Email, User
 from .forms import (
     LoginForm,
     PasswordResetForm,
@@ -38,7 +38,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter(Email.email==form.email.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid email address or password')
             return redirect(url_for('auth.login'))
