9f4ef1b097242741cca22b3a100c993adeea0a6c
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-17 21:07:00 -0500

    Adds a verify your email function.

--- app/blueprints/auth/mail.py
+++ app/blueprints/auth/mail.py
@@ -11,6 +11,14 @@ from flask import current_app, render_template
 from app.mail import send_mail
 
 
+def send_email_verification_mail(user):
+    token = user.password_reset_token
+    send_mail('[SPA-Base] Verify Your Email',
+              sender=current_app.config['SERVER_EMAIL'], recipients=[user.email],
+              text_body=render_template('auth/email/email_verification.txt', user=user, token=token),
+              html_body=render_template('auth/email/email_verification.html',
+                                        user=user, token=token))
+
 def send_password_reset_mail(user):
     token = user.password_reset_token
     send_mail('[SPA-Base] Reset Your Password',
