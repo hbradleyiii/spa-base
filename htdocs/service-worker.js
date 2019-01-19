31f16fbd9a470c330a3c3bec735095b64bb916ae
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-19 15:37:57 -0500

    Adds initial code to service worker for page caching.

---
+++ htdocs/service-worker.js
@@ -0,0 +1,46 @@
+/**
+ * SPA Base Service Worker
+ */
+
+var version = 0.1;
+
+// Offline cache name
+var cache_name = 'spa_base-' + version;
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+		// Offline Caching
+		caches.open(cache_name).then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/css/app.css',
+				'/css/auth.css',
+				'/icons/logo-32x32.png',
+				'/icons/zondicons.svg',
+				'/js/show-password.js',
+				'/login/',
+				'/manifest.webmanifest',
+				'/service-worker.js',
+				'https://fonts.googleapis.com/css?family=Libre+Franklin:400,400i,700',
+				'https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700',
+			])
+			.then(function() { self.skipWaiting(); });
+		})
+	);
+});
+
+
+self.addEventListener('activate', function(event) {
+	return event.waitUntil(self.clients.claim());
+});
+
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open(cache_name)
+			.then(function(cache) { return cache.match(event.request, {ignoreSearch: true}); })
+			.then(function(response) {
+				return response || fetch(event.request);
+			})
+	);
+});
