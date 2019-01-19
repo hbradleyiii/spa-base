5d115a170e5aabcb4d8216167752180e5abcb558
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-18 19:23:23 -0500

    Adds settings to login manager.

--- app/blueprints/auth/login.py
+++ app/blueprints/auth/login.py
@@ -13,7 +13,14 @@ from app.models import User
 
 
 login = LoginManager()
+
 login.login_view = 'auth.login'
+login.refresh_view = 'auth.reauthenticate'
+login.login_message = u'Please log in to access this page.'
+login.session_protection = 'strong'
+login.needs_refresh_message = (
+    u'To protect your account, please reauthenticate to access this page.'
+)
 
 @login.user_loader
 def load_user(id):
