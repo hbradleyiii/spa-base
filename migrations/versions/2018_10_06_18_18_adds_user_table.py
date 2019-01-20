6e336bb39b2f9ae6a32e645c3c13524c90034d8a
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-19 22:39:13 -0500

    Adds triggers to database migrations. Also makes a few tweaks in code.

--- migrations/versions/2018_10_06_18_18_adds_user_table.py
+++ migrations/versions/2018_10_06_18_18_adds_user_table.py
@@ -23,29 +23,64 @@ def upgrade():
     op.create_table('users',
         sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
         sa.Column('password_hash', sa.String(length=128), nullable=False),
-        sa.Column('first_name', sa.String(length=128), nullable=True),
-        sa.Column('last_name', sa.String(length=128), nullable=True),
-        sa.Column('primary_email', sa.String(length=128), nullable=True),
+        sa.Column('first_name', sa.String(length=128), nullable=False),
+        sa.Column('last_name', sa.String(length=128), nullable=False),
+        sa.Column('primary_email_fk', sa.String(length=128), nullable=True),
         sa.Column('active', sa.Boolean(), nullable=True),
         sa.PrimaryKeyConstraint('id'),
-        sa.UniqueConstraint('primary_email')
+        sa.UniqueConstraint('primary_email_fk')
     )
 
     op.create_table('emails',
         sa.Column('email', sa.String(length=128), nullable=False),
         sa.Column('user_id', sa.Integer(), nullable=False),
         sa.Column('verified', sa.Boolean(), nullable=True),
-        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'],
+                                name='fk_email_user_id', ondelete='CASCADE'),
         sa.PrimaryKeyConstraint('email', 'user_id')
     )
-    op.create_index(op.f('ix_emails_email'), 'emails', ['email'], unique=False)
+    op.create_index(op.f('ix_emails_email'), 'emails', ['email'], unique=True)
 
-    op.create_foreign_key('fk_user_primary_email', 'users', 'emails',
-                          ['id', 'primary_email'], ['user_id', 'email'],
+    op.create_foreign_key('fk_user_id_primary_email', 'users', 'emails',
+                          ['primary_email_fk', 'id'], ['email', 'user_id'],
                           ondelete='RESTRICT')
+
+    connection = op.get_bind()
+
+    connection.execute("""
+        CREATE TRIGGER enforce_verified_primary_email_on_update BEFORE UPDATE ON users
+            FOR EACH ROW
+            BEGIN
+                IF (SELECT COUNT(email) FROM emails
+                        WHERE emails.email = NEW.primary_email_fk
+                        AND emails.verified = 1) != 1
+                THEN
+                    SIGNAL SQLSTATE '45000' SET message_text = 'Primary email must exist in emails table and be a verified email.';
+                END IF;
+            END;
+    """)
+
+    connection.execute("""
+        CREATE TRIGGER enforce_email_immutability_on_update BEFORE UPDATE ON emails
+            FOR EACH ROW
+            BEGIN
+                IF NEW.email != OLD.email OR NEW.user_id != OLD.user_id
+                THEN
+                    SIGNAL SQLSTATE '45000' SET message_text = 'Email is immutable and cannot be changed.';
+                END IF;
+            END;
+    """)
+
 
 def downgrade():
     op.drop_index(op.f('ix_emails_email'), table_name='emails')
-    op.drop_constraint(None, 'users', type_='foreignkey')
+    op.drop_constraint('fk_email_user_id', 'emails', type_='foreignkey')
+    op.drop_constraint('fk_user_id_primary_email', 'users', type_='foreignkey')
+
+    connection = op.get_bind()
+
+    connection.execute("DROP TRIGGER IF EXISTS enforce_verified_primary_email_on_update")
+    connection.execute("DROP TRIGGER IF EXISTS enforce_email_immutability_on_update")
+
     op.drop_table('users')
     op.drop_table('emails')
