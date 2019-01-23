83f2bff8dfbbb621e17d8585225662dfaf77a6b9
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-22 20:17:24 -0500

    Adds clarification to comment.

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -190,7 +190,7 @@ def test_user_can_set_a_primary_email_from_their_emails(session):
     assert user.primary_email == 'jane1@example.com'
     assert user.email == 'jane1@example.com'
 
-    # When setting the primary email via the `email` property
+    # When setting the primary email via the `email` property to a verified email
     user.email = 'jane2@example.com'
     session.commit()
 
