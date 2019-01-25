d74486db26044168419e28e0e0403c5c88fa6991
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-24 20:05:26 -0500

    Expands test client for cleaner tests.

--- tests/utilities/client.py
+++ tests/utilities/client.py
@@ -10,10 +10,13 @@ The idea was derived from:
 @see https://gist.github.com/singingwolfboy/2fca1de64950d5dfed72
 """
 
-from flask import g
+from flask import g, url_for
 from flask import session as flask_session
 from flask.testing import FlaskClient
 from flask_wtf.csrf import generate_csrf
+from werkzeug.routing import BuildError
+
+from .helpers import create_user
 
 
 class Client(FlaskClient):
@@ -28,6 +31,18 @@ class Client(FlaskClient):
             kwargs['data']['csrf_token'] = self.csrf_token
         return super(Client, self).post(*args, **kwargs)
 
+    def open(self, path, *args, **kwargs):
+        """Wraps the client open method to allow passing flask route strings
+        for cleaner test code."""
+        path = self.get_url(path)
+        return super(Client, self).open(path, *args, **kwargs)
+
+    def get_url(self, string):
+        try:
+            return url_for(string)
+        except BuildError:
+            return string
+
     def generate_csrf(self):
         """Generates a signed csrf token and stores the raw token in the
         current session."""
@@ -38,3 +53,10 @@ class Client(FlaskClient):
             csrf = generate_csrf()
             session['csrf_token'] = flask_session['csrf_token']
         return csrf
+
+    def login(self, email, password):
+        return self.post(url_for('auth.login'), data=dict(email='jane@example.com',
+                                                          password='password123'))
+    def login_new_user(self, session):
+        user = create_user(session, password='password123')
+        return self.login(email=user.email, password='password123')
