113912da808ce3f8c7e367cac9d44118d438df28
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-01-25 22:45:16 -0500

    Spacing issues.

--- app/blueprints/auth/mail.py
+++ app/blueprints/auth/mail.py
@@ -15,7 +15,8 @@ def send_email_verification_mail(user, email):
     token = email.verification_token
     send_mail('[SPA-Base] Verify Your Email',
               sender=current_app.config['SERVER_EMAIL'], recipients=[email.email],
-              text_body=render_template('auth/email/email_verification.txt', user=user, token=token),
+              text_body=render_template('auth/email/email_verification.txt',
+                                        user=user, token=token),
               html_body=render_template('auth/email/email_verification.html',
                                         user=user, token=token))
 
@@ -23,7 +24,8 @@ def send_password_reset_mail(user):
     token = user.password_reset_token
     send_mail('[SPA-Base] Reset Your Password',
               sender=current_app.config['SERVER_EMAIL'], recipients=[user.email],
-              text_body=render_template('auth/email/password_reset.txt', user=user, token=token),
+              text_body=render_template('auth/email/password_reset.txt',
+                                        user=user, token=token),
               html_body=render_template('auth/email/password_reset.html',
                                         user=user, token=token))
 
